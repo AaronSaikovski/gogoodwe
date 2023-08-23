55718ae91821cc831ed48e2ba0935490c5073e2a
AaronSaikovski <asaikovski@outlook.com>
2023-08-23 17:28:26 +1000

    updated version tag

--- pkg/goodwe/constants/constants.go
+++ pkg/goodwe/constants/constants.go
@@ -11,7 +11,7 @@ const (
 	HTTPTimeout = 20
 
 	// Version string
-	VersionString string = "gogoodwe v1.2.0"
+	VersionString string = "gogoodwe v1.3.1"
 
 	//API login success response message
 	SemsLoginSuccessResponse string = "Successful"
