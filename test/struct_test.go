84822ef44dcf1fcd0e173df5452495619af308a1
aaronsaikovski <asaikovski@outlook.com>
2024-02-06 13:06:03 +1100

    fixed refactor

--- test/struct_test.go
+++ test/struct_test.go
@@ -5,15 +5,15 @@ A Sample test harness.
 package testing
 
 // import (
-// 	"github.com/AaronSaikovski/gostarter/internal/app/types"
+// 	"github.com/AaronSaikovski/gogoodwe/cmd/gogoodwe/types"
 // 	"testing"
 // )
 
 // // A testing function.
-// func TestSampleStructString(t *testing.T) {
+// func TestLoginCredentials(t *testing.T) {
 
 // 	expected := "test data"
-// 	ateststruct := types.Sample{SampleString: "test data", SampleInt: 1}
+// 	ateststruct := types.LoginCredentials{SampleString: "test data", SampleInt: 1}
 
 // 	if ateststruct.SampleString != expected {
 // 		t.Errorf("struct expected '%s' but got '%s'", expected, ateststruct.SampleString)
